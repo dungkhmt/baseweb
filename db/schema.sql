4d0a350bf14e9f155a288d9620525c891e9b46ac
tuandat95cbn <tuandat95cbn@gmail.com>
2019-11-02 01:41:04 +0700

    update schema database

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE status_type (
   status_type_id     VARCHAR(60) NOT NULL,
   parent_type_id     VARCHAR(60),
-  description        VARCHAR(10000),
+  description        TEXT,
   last_updated_stamp TIMESTAMP   NULL ON UPDATE CURRENT_TIMESTAMP,
   created_stamp      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT pk_status_type PRIMARY KEY (status_type_id),
@@ -12,7 +12,7 @@ CREATE TABLE status (
   status_type_id     VARCHAR(60),
   status_code        VARCHAR(60),
   sequence_id        VARCHAR(60),
-  description        VARCHAR(10000),
+  description        TEXT,
   last_updated_stamp TIMESTAMP   NULL ON UPDATE CURRENT_TIMESTAMP,
   created_stamp      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT pk_status PRIMARY KEY (status_id),
@@ -22,7 +22,7 @@ CREATE TABLE party_type (
   party_type_id      VARCHAR(60) NOT NULL,
   parent_type_id     VARCHAR(60),
   has_table          BOOLEAN,
-  description        VARCHAR(10000),
+  description        TEXT,
   last_updated_stamp TIMESTAMP   NULL ON UPDATE CURRENT_TIMESTAMP,
   created_stamp      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT pk_party_type PRIMARY KEY (party_type_id),
@@ -44,16 +44,27 @@ CREATE TABLE party (
   party_code                  VARCHAR(255),
   CONSTRAINT pk_party PRIMARY KEY (party_id),
   CONSTRAINT party_statusitm FOREIGN KEY (status_id) REFERENCES status (status_id),
-  CONSTRAINT party_pty_typ FOREIGN KEY (party_type_id) REFERENCES party_type (party_type_id),
-  CONSTRAINT party_m_user_login FOREIGN KEY (last_modified_by_user_login) REFERENCES user_login (user_login_id),
-  CONSTRAINT party_c_user_login FOREIGN KEY (created_by_user_login) REFERENCES user_login (user_login_id)
+  CONSTRAINT party_pty_typ FOREIGN KEY (party_type_id) REFERENCES party_type (party_type_id)
 );
+CREATE TABLE person (
+  party_id                    VARCHAR(60)              NOT NULL,
+  first_name                  VARCHAR(100),
+  middle_name                 VARCHAR(100),
+  last_name                   VARCHAR(100),
+  gender                      CHARACTER(1),
+  birth_date                  DATE,
+  last_updated_stamp          TIMESTAMP                NULL,
+  created_stamp               TIMESTAMP,
+  CONSTRAINT pk_person PRIMARY KEY (party_id),
+  CONSTRAINT person_party FOREIGN KEY (party_id) REFERENCES party (party_id)
+);
+
 CREATE TABLE user_login (
   user_login_id            VARCHAR(255)  NOT NULL,
   current_password         VARCHAR(60),
   otp_secret               VARCHAR(60),
   client_token             VARCHAR(512),
-  password_hint            VARCHAR(10000),
+  password_hint            TEXT,
   is_system                BOOLEAN,
   enabled                  BOOLEAN,
   has_logged_out           BOOLEAN,
@@ -67,16 +78,22 @@ CREATE TABLE user_login (
   CONSTRAINT pk_user_login PRIMARY KEY (user_login_id),
   CONSTRAINT user_party FOREIGN KEY (party_id) REFERENCES party (party_id)
 );
+
+ALTER TABLE party
+  ADD CONSTRAINT party_m_user_login FOREIGN KEY (last_modified_by_user_login) REFERENCES user_login (user_login_id);
+ALTER TABLE party
+  ADD CONSTRAINT party_c_user_login FOREIGN KEY (created_by_user_login) REFERENCES user_login (user_login_id);
+
 CREATE TABLE security_group (
   group_id           VARCHAR(60) NOT NULL,
-  description        VARCHAR(10000),
+  description        TEXT,
   last_updated_stamp TIMESTAMP   NULL ON UPDATE CURRENT_TIMESTAMP,
   created_stamp      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT pk_security_group PRIMARY KEY (group_id)
 );
 CREATE TABLE security_permission (
   permission_id      VARCHAR(100) NOT NULL,
-  description        VARCHAR(10000),
+  description        TEXT,
   last_updated_stamp TIMESTAMP    NULL ON UPDATE CURRENT_TIMESTAMP,
   created_stamp      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT pk_security_permission PRIMARY KEY (permission_id)
@@ -98,4 +115,26 @@ CREATE TABLE user_login_security_group (
   CONSTRAINT pk_user_login_security_group PRIMARY KEY (user_login_id, group_id),
   CONSTRAINT user_secgrp_grp FOREIGN KEY (group_id) REFERENCES security_group (group_id),
   CONSTRAINT user_secgrp_user FOREIGN KEY (user_login_id) REFERENCES user_login (user_login_id)
+);
+
+CREATE TABLE application_type (
+  application_type_id VARCHAR(60) NOT NULL,
+  description         TEXT,
+  last_updated_stamp  TIMESTAMP   NULL ON UPDATE CURRENT_TIMESTAMP,
+  created_stamp       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT pk_application_type PRIMARY KEY (application_type_id)
+);
+
+CREATE TABLE application (
+  application_id      VARCHAR(255) NOT NULL,
+  application_type_id VARCHAR(255) NOT NULL,
+  module_id           VARCHAR(255),
+  permission_id       VARCHAR(255),
+  description         TEXT,
+  last_updated_stamp  TIMESTAMP    NULL ON UPDATE CURRENT_TIMESTAMP,
+  created_stamp       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT pk_application PRIMARY KEY (application_id),
+  CONSTRAINT application_application_type FOREIGN KEY (application_type_id) REFERENCES application_type (application_type_id),
+  CONSTRAINT application_application_module FOREIGN KEY (module_id) REFERENCES application (application_id),
+  CONSTRAINT application_permission FOREIGN KEY (permission_id) REFERENCES security_permission (permission_id)
 );
---
+++ db/seed.sql
@@ -0,0 +1,56 @@
+INSERT INTO party_type (party_type_id, parent_type_id, has_table, description, last_updated_stamp, created_stamp) VALUES ('AUTOMATED_AGENT', NULL, FALSE, 'Automated Agent', NOW(), NOW());
+INSERT INTO party_type (party_type_id, parent_type_id, has_table, description, last_updated_stamp, created_stamp) VALUES ( 'PERSON', NULL, TRUE, 'Person', NOW(), NOW());
+INSERT INTO party_type (party_type_id, parent_type_id, has_table, description, last_updated_stamp, created_stamp) VALUES ( 'PARTY_GROUP', NULL, TRUE, 'Party Group', NOW(), NOW());
+INSERT INTO party_type (party_type_id, parent_type_id, has_table, description, last_updated_stamp, created_stamp) VALUES ( 'BANK', 'PARTY_GROUP', TRUE, 'Bank', NOW(), NOW());
+INSERT INTO party_type (party_type_id, parent_type_id, has_table, description, last_updated_stamp, created_stamp) VALUES ( 'LEGAL_ORGANIZATION', 'PARTY_GROUP', FALSE, 'Legal Organization', '2017-01-03 10:11:27.885', '2017-01-03 10:11:27.608');
+INSERT INTO party_type (party_type_id, parent_type_id, has_table, description, last_updated_stamp, created_stamp) VALUES ( 'CORPORATION', 'LEGAL_ORGANIZATION', FALSE, 'Corporation', NOW(), NOW());
+INSERT INTO party_type (party_type_id, parent_type_id, has_table, description, last_updated_stamp, created_stamp) VALUES ( 'CUSTOMER_GROUP', 'PARTY_GROUP', FALSE, 'Customer Group', NOW(), NOW());
+INSERT INTO status_type (status_type_id, parent_type_id, description, last_updated_stamp, created_stamp) VALUES ( 'PARTY_STATUS', NULL, 'Party status', NOW(), NOW());
+INSERT INTO status_type (status_type_id, parent_type_id, description, last_updated_stamp, created_stamp) VALUES ('MARRY_STATUS', NULL, 'Marry status', NOW(), NOW());
+INSERT INTO status_type (status_type_id, parent_type_id, description, last_updated_stamp, created_stamp) VALUES ( 'SERVICE_STATUS', NULL, 'Service status', NOW(), NOW());
+INSERT INTO status (status_id, status_type_id, status_code, sequence_id, description, last_updated_stamp, created_stamp) VALUES ('SINGLE', 'MARRY_STATUS', 'SINGLE', 0, 'Độc thân', NOW(), NOW());
+INSERT INTO status (status_id, status_type_id, status_code, sequence_id, description, last_updated_stamp, created_stamp) VALUES ('MARRIED', 'MARRY_STATUS', 'MARRIED', 0, 'Đã kết hôn', NOW(), NOW());
+INSERT INTO status (status_id, status_type_id, status_code, sequence_id, description, last_updated_stamp, created_stamp) VALUES ('DIVORCED', 'MARRY_STATUS', 'DIVORCED', 0, 'Đã ly dị', NOW(), NOW());
+INSERT INTO status (status_id, status_type_id, status_code, sequence_id, description, last_updated_stamp, created_stamp) VALUES ('PARTY_ENABLED', 'PARTY_STATUS', 'ENABLED', 0, 'Đã kích hoạt', NOW(), NOW());
+INSERT INTO status (status_id, status_type_id, status_code, sequence_id, description, last_updated_stamp, created_stamp) VALUES ('PARTY_DISABLED', 'PARTY_STATUS', 'DISABLED', 0, 'Đã bị vô hiệu hóa', NOW(), NOW());
+
+INSERT INTO security_group (group_id, description, last_updated_stamp, created_stamp) VALUES ('ROLE_SALE_MANAGER', 'Sale manager account owner access security group', '2017-01-03 10:12:23.879', '2017-01-03 10:12:23.878');
+INSERT INTO security_group (group_id, description, last_updated_stamp, created_stamp) VALUES ('ROLE_ACCOUNTANT', 'Accountant account owner access security group', '2017-01-03 10:12:42.531', '2017-01-03 10:12:42.507');
+INSERT INTO security_group (group_id, description, last_updated_stamp, created_stamp) VALUES ('ROLE_FULL_ADMIN', 'Full Admin group, has all general functional permissions.', '2017-01-03 10:12:23.994', '2017-01-03 10:12:23.993');
+INSERT INTO security_permission (permission_id, description, last_updated_stamp, created_stamp) VALUES ('USER_ADMIN','Admin user permission',NOW(),NOW());
+INSERT INTO security_permission (permission_id, description, last_updated_stamp, created_stamp) VALUES ('USER_CREATE','Create user permission',NOW(),NOW());
+INSERT INTO security_permission (permission_id, description, last_updated_stamp, created_stamp) VALUES ('USER_VIEW','View user permission',NOW(),NOW());
+INSERT INTO security_permission (permission_id, description, last_updated_stamp, created_stamp) VALUES ('ORDER_ADMIN','Admin order permission',NOW(),NOW());
+INSERT INTO security_permission (permission_id, description, last_updated_stamp, created_stamp) VALUES ('ORDER_CREATE','Create order permission',NOW(),NOW());
+INSERT INTO security_permission (permission_id, description, last_updated_stamp, created_stamp) VALUES ('ORDER_VIEW','View order permission',NOW(),NOW());
+INSERT INTO security_permission (permission_id, description, last_updated_stamp, created_stamp) VALUES ('INVOICE_ADMIN','Admin order permission',NOW(),NOW());
+INSERT INTO security_permission (permission_id, description, last_updated_stamp, created_stamp) VALUES ('INVOICE_CREATE','Create order permission',NOW(),NOW());
+INSERT INTO security_permission (permission_id, description, last_updated_stamp, created_stamp) VALUES ('INVOICE_VIEW','View order permission',NOW(),NOW());
+INSERT INTO security_group_permission(group_id, permission_id, last_updated_stamp, created_stamp) VALUES ('ROLE_FULL_ADMIN','ORDER_ADMIN',NOW(),NOW());
+INSERT INTO security_group_permission(group_id, permission_id, last_updated_stamp, created_stamp) VALUES ('ROLE_FULL_ADMIN','USER_ADMIN',NOW(),NOW());
+INSERT INTO security_group_permission(group_id, permission_id, last_updated_stamp, created_stamp) VALUES ('ROLE_FULL_ADMIN','INVOICE_ADMIN',NOW(),NOW());
+INSERT INTO security_group_permission(group_id, permission_id, last_updated_stamp, created_stamp) VALUES ('ROLE_SALE_MANAGER','ORDER_ADMIN',NOW(),NOW());
+INSERT INTO security_group_permission(group_id, permission_id, last_updated_stamp, created_stamp) VALUES ('ROLE_ACCOUNTANT','ORDER_VIEW',NOW(),NOW());
+INSERT INTO security_group_permission(group_id, permission_id, last_updated_stamp, created_stamp) VALUES ('ROLE_ACCOUNTANT','INVOICE_ADMIN',NOW(),NOW());
+INSERT INTO application_type(application_type_id, description, last_updated_stamp, created_stamp) VALUES ('MENU','Menu application type', NOW(),NOW());
+INSERT INTO application_type(application_type_id, description, last_updated_stamp, created_stamp) VALUES ('SCREEN','Screen application type', NOW(),NOW());
+INSERT INTO application_type(application_type_id, description, last_updated_stamp, created_stamp) VALUES ('MODULE','Module application type', NOW(),NOW());
+INSERT INTO application_type(application_type_id, description, last_updated_stamp, created_stamp) VALUES ('SERVICE','Service application type', NOW(),NOW());
+INSERT INTO application_type(application_type_id, description, last_updated_stamp, created_stamp) VALUES ('ENTITY','Entity application type', NOW(),NOW());
+INSERT INTO application (application_id, application_type_id, module_id, permission_id, description, last_updated_stamp,created_stamp) VALUES ('MENU_USER','MENU',NULL ,NULL,'Menu user management',NOW(), NOW());
+INSERT INTO application (application_id, application_type_id, module_id, permission_id, description, last_updated_stamp,created_stamp) VALUES ('MENU_USER_CREATE','MENU','MENU_USER' ,'USER_CREATE','Menu user create',NOW(), NOW());
+INSERT INTO application (application_id, application_type_id, module_id, permission_id, description, last_updated_stamp,created_stamp) VALUES ('MENU_USER_LIST','MENU','MENU_USER' ,'USER_VIEW','Menu user list',NOW(), NOW());
+INSERT INTO application (application_id, application_type_id, module_id, permission_id, description, last_updated_stamp,created_stamp) VALUES ('MENU_ORDER','MENU',NULL ,NULL,'Menu order management',NOW(), NOW());
+INSERT INTO application (application_id, application_type_id, module_id, permission_id, description, last_updated_stamp,created_stamp) VALUES ('MENU_ORDER_LIST','MENU','MENU_ORDER' ,'ORDER_VIEW','Menu order list',NOW(), NOW());
+INSERT INTO application (application_id, application_type_id, module_id, permission_id, description, last_updated_stamp,created_stamp) VALUES ('MENU_ORDER_CREATE','MENU','MENU_ORDER' ,'ORDER_CREATE','Menu order create',NOW(), NOW());
+INSERT INTO application (application_id, application_type_id, module_id, permission_id, description, last_updated_stamp,created_stamp) VALUES ('MENU_INVOICE','MENU',NULL ,NULL,'Menu invoice management',NOW(), NOW());
+INSERT INTO application (application_id, application_type_id, module_id, permission_id, description, last_updated_stamp,created_stamp) VALUES ('MENU_INVOICE_CREATE','MENU','MENU_INVOICE' ,'INVOICE_CREATE','Menu invoice create',NOW(), NOW());
+INSERT INTO application (application_id, application_type_id, module_id, permission_id, description, last_updated_stamp,created_stamp) VALUES ('MENU_INVOICE_LIST','MENU','MENU_INVOICE' ,'INVOICE_VIEW','Menu invoice list',NOW(), NOW());
+
+INSERT INTO party (party_id, party_type_id, external_id, description, status_id, created_date, created_by_user_login, last_modified_date, last_modified_by_user_login, is_unread, last_updated_stamp, created_stamp, party_code) VALUES ('admin', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, FALSE, NOW(), NOW(), NULL);
+INSERT  INTO person (party_id, first_name, middle_name, last_name, gender, birth_date, last_updated_stamp, created_stamp) VALUES ('admin','admin',',',',','M',NOW(),null,NOW());
+INSERT INTO user_login (user_login_id, current_password, password_hint, is_system, enabled, has_logged_out, require_password_change, disabled_date_time, successive_failed_logins, last_updated_stamp, created_stamp, party_id) VALUES ( 'admin', '$2a$10$3l5d7fPeBuwUSbXYBiSiOuohtbgyDu2J/C5TWGHmszoD4a6mKFmey', NULL, FALSE, TRUE, FALSE, FALSE, NULL, NULL, NOW(), NOW(), 'admin');
+INSERT INTO user_login_security_group (user_login_id, group_id, last_updated_stamp, created_stamp) VALUES ('admin', 'ROLE_FULL_ADMIN', NOW(), NOW());
+
+
+
