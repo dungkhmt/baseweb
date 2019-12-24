f95452aca37fa17e8095f3e7e07e4f010bb14df9
tuandat95cbn <tuandat95cbn@gmail.com>
2019-12-24 11:34:21 +0700

    fix bug login

--- src/main/java/com/hust/baseweb/config/SecurityConfig.java
+++ src/main/java/com/hust/baseweb/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.hust.baseweb.entity.UserLogin;
 import com.hust.baseweb.service.BaseWebUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -26,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-
+                .antMatchers(HttpMethod.OPTIONS, "**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
--- src/main/java/com/hust/baseweb/controller/ApiController.java
+++ src/main/java/com/hust/baseweb/controller/ApiController.java
@@ -1,21 +1,27 @@
 package com.hust.baseweb.controller;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
+@CrossOrigin
 public class ApiController {
-   @GetMapping("/")
-    public ResponseEntity<Map> home(Principal principal){
-       Map<String,String> res= new HashMap<>();
-       res.put("user",principal.getName());
-       return new ResponseEntity<Map>(res, HttpStatus.CREATED);
-   }
+    @GetMapping("/")
+    public ResponseEntity<Map> home(Principal principal) {
+        Map<String, String> res = new HashMap<>();
+        res.put("user", principal.getName());
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Access-Control-Expose-Headers","X-Auth-Token");
+        return ResponseEntity.ok().headers(responseHeaders).body(res);
+    }
 }
 
