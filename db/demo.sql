4c4e2a3baae72346af547a69ba933724c7f44b5e
tuandat95cbn <tuandat95cbn@gmail.com>
2020-01-07 11:02:45 +0700

    fix bug init sql

--- db/demo.sql
+++ db/demo.sql
@@ -11,5 +11,5 @@ VALUES ('9a8e40d2-265c-11ea-acaa-eb83ca2329f6', 'PERSON', NULL, NULL, 'PARTY_ENA
 INSERT  INTO person (party_id, first_name, middle_name, last_name, gender, birth_date, last_updated_stamp, created_stamp)
 VALUES ('9a8e40d2-265c-11ea-acaa-eb83ca2329f6','Trần','Thị','Toán','M',NOW(),null,NOW());
 INSERT INTO user_login (user_login_id, current_password, password_hint, is_system, enabled, has_logged_out, require_password_change, disabled_date_time, successive_failed_logins, last_updated_stamp, created_stamp, party_id)
-VALUES ( 'tranthitoan', '$$2a$04$cqFXgdkB.8u2HwT3QUTVZuePtHdzi.rWFCjdgNbVB7l6vn/yAU7F6', NULL, FALSE, TRUE, FALSE, FALSE, NULL, NULL, NOW(), NOW(), '9a8e40d2-265c-11ea-acaa-eb83ca2329f6');
+VALUES ( 'tranthitoan', '$2a$04$cqFXgdkB.8u2HwT3QUTVZuePtHdzi.rWFCjdgNbVB7l6vn/yAU7F6', NULL, FALSE, TRUE, FALSE, FALSE, NULL, NULL, NOW(), NOW(), '9a8e40d2-265c-11ea-acaa-eb83ca2329f6');
 INSERT INTO user_login_security_group (user_login_id, group_id, last_updated_stamp, created_stamp) VALUES ('tranthitoan', 'ROLE_ACCOUNTANT', NOW(), NOW());
