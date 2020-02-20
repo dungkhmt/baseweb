60949cf4a11f120ef24f2fb3e258bda738a9f124
tuandat95cbn <tuandat95cbn@gmail.com>
2020-02-20 18:14:36 +0700

    fix conflict

--- src/main/java/com/hust/baseweb/service/UserService.java
+++ src/main/java/com/hust/baseweb/service/UserService.java
@@ -1,14 +1,8 @@
 package com.hust.baseweb.service;
 
-<<<<<<< f08dc7070064a7308996696a19e3e7fc13924f73
-=======
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
->>>>>>> update edit user
 import com.hust.baseweb.entity.Party;
 import com.hust.baseweb.entity.UserLogin;
 import com.hust.baseweb.model.PersonModel;
@@ -16,10 +10,9 @@ import com.hust.baseweb.model.PersonUpdateModel;
 import com.hust.baseweb.model.querydsl.SortAndFiltersInput;
 import com.hust.baseweb.rest.user.DPerson;
 import com.hust.baseweb.rest.user.UserRestBriefProjection;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 
 public interface UserService {
     public UserLogin findById(String userLoginId);
@@ -32,12 +25,7 @@ public interface UserService {
 
     public List<UserLogin> getAllUserLogins();
 
-    public UserLogin save(String userName, String password) throws Exception;
-<<<<<<< f08dc7070064a7308996696a19e3e7fc13924f73
-
     public Party save(PersonModel personModel, String createdBy) throws Exception;
-=======
-    public Party save(PersonModel personModel,String createdBy) throws Exception;
-    public Party update(PersonUpdateModel personUpdateModel,UUID partyId,String updateBy); 
->>>>>>> update edit user
+
+    public Party update(PersonUpdateModel personUpdateModel, UUID partyId, String updateBy);
 }
