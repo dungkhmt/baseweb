c3f7f0f6f39a4006ffe4fe994c1ec9352afc86f8
tuandat95cbn <tuandat95cbn@gmail.com>
2020-02-21 02:25:04 +0700

    move redis config to .yml file

--- src/main/java/com/hust/baseweb/config/SessionConfig.java
+++ src/main/java/com/hust/baseweb/config/SessionConfig.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -23,11 +21,6 @@ public class SessionConfig {
     @Autowired
     public void setRedisConnectionFactory(RedisConnectionFactory redisConnectionFactory) {
         this.redisConnectionFactory = redisConnectionFactory;
-    }
-
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("localhost", 6379));
     }
 
     @Bean
