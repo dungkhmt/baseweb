e2409afcf6057cf15d6f2154d39b3517f9abcca2
dungkhmt <dungkhmt@gmail.com>
2020-03-09 00:18:01 +0700

    upgrade facility and delivery_plan db

--- src/main/java/com/hust/baseweb/applications/logistics/db/schema_product.sql
+++ src/main/java/com/hust/baseweb/applications/logistics/db/schema_product.sql
@@ -122,6 +122,7 @@ CREATE TABLE facility
     facility_type_id   VARCHAR(60),
     parent_facility_id VARCHAR(60),
     facility_name      VARCHAR(100),
+    contact_mech_id 	UUID,
     product_store_id   VARCHAR(60),
     opened_date        TIMESTAMP,
     closed_date        TIMESTAMP,
@@ -129,6 +130,7 @@ CREATE TABLE facility
     last_updated_stamp TIMESTAMP,
     created_stamp      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pk_facility_id PRIMARY KEY (facility_id),
+    constraint fk_facility_contact_mech_id foreign key(contact_mech_id) references postal_address(contact_mech_id),
     CONSTRAINT fk_facility_type_id FOREIGN KEY (facility_type_id) REFERENCES facility_type (facility_type_id),
     CONSTRAINT fk_parent_facility_id FOREIGN KEY (parent_facility_id) REFERENCES facility (facility_id),
     CONSTRAINT fk_product_store_id FOREIGN KEY (product_store_id) REFERENCES product_store (product_store_id)
