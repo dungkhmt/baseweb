c5e7c13bcdc637315774a98fb49fd6ecab3a96db
AnhTuan-AiT <58069686+AnhTuan-AiT@users.noreply.github.com>
2020-05-01 12:23:35 +0700

    add method findAllByPartyIdNotIn

--- src/main/java/com/hust/baseweb/applications/customer/repo/PartyRetailOutletRepo.java
+++ src/main/java/com/hust/baseweb/applications/customer/repo/PartyRetailOutletRepo.java
@@ -4,6 +4,7 @@ import com.hust.baseweb.applications.customer.entity.PartyRetailOutlet;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,5 +14,6 @@ public interface PartyRetailOutletRepo extends JpaRepository<PartyRetailOutlet, 
     Page<PartyRetailOutlet> findAll(Pageable page);
 
     List<PartyRetailOutlet> findAllByPartyIdIn(List<UUID> partyIds);
+    List<PartyRetailOutlet> findAllByPartyIdNotIn(List<UUID> partyIds);
     Page<PartyRetailOutlet> findAllByPartyIdIn(List<UUID> partyIds, Pageable page);
 }
