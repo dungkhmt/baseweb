7cc677ac3bd8e7bd98eb31a486d2692c43914bc9
AnhTuan-AiT <anhtuan0126104@gmail.com>
2020-05-18 19:08:28 +0700

    + add method: getSalesRouteDetailOfPlanPeriod

--- src/main/java/com/hust/baseweb/applications/salesroutes/service/SalesRouteDetailService.java
+++ src/main/java/com/hust/baseweb/applications/salesroutes/service/SalesRouteDetailService.java
@@ -2,6 +2,7 @@ package com.hust.baseweb.applications.salesroutes.service;
 
 import com.hust.baseweb.applications.customer.entity.PartyCustomer;
 import com.hust.baseweb.applications.customer.entity.PartyRetailOutlet;
+import com.hust.baseweb.applications.salesroutes.repo.SalesRouteDetailRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,4 +13,7 @@ public interface SalesRouteDetailService {
     int generateSalesRouteDetailOfSalesman(UUID partySalesmanId, UUID salesRoutePlanningPeriodId);
 
     List<PartyRetailOutlet> getRetailOutletsVisitedSalesmanDay(UUID partySalesmanId, String date);
+
+    List<SalesRouteDetailRepo
+            .GetSalesRouteDetailOfPlanPeriodOutputModel> getSalesRouteDetailOfPlanPeriod(UUID salesRoutePlanningPeriodId);
 }
