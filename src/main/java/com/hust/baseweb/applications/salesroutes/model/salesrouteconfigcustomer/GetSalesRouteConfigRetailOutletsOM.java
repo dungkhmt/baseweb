a8db167ee59f895c769f357cdf0069443ee63a2f
AnhTuan-AiT <anhtuan0126104@gmail.com>
2020-06-10 14:31:31 +0700

    - output model for method: getSalesRouteConfigRetailOutlets

---
+++ src/main/java/com/hust/baseweb/applications/salesroutes/model/salesrouteconfigcustomer/GetSalesRouteConfigRetailOutletsOM.java
@@ -0,0 +1,13 @@
+package com.hust.baseweb.applications.salesroutes.model.salesrouteconfigcustomer;
+
+public interface GetSalesRouteConfigRetailOutletsOM {
+    String getSalesRouteConfigRetailOutletId();
+    String getRetailOutletCode();
+    String getRetailOutletName();
+    String getSalesmanName();
+    String getPartySalesmanId();
+    String getDistributorName();
+    String getVisitFrequency();
+    String getVisitConfig();
+    int getRepeatWeek();
+}
