25ac05e299d04a79c3856b71790c3c4cffc34c4d
AnhTuan-AiT <anhtuan0126104@gmail.com>
2020-06-10 14:38:51 +0700

    - add: method getCurrentPlanPeriod

--- src/main/java/com/hust/baseweb/applications/salesroutes/service/SalesRoutePlanningPeriodService.java
+++ src/main/java/com/hust/baseweb/applications/salesroutes/service/SalesRoutePlanningPeriodService.java
@@ -3,6 +3,7 @@ package com.hust.baseweb.applications.salesroutes.service;
 import com.hust.baseweb.applications.salesroutes.entity.SalesRoutePlanningPeriod;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,4 +15,6 @@ public interface SalesRoutePlanningPeriodService {
     List<SalesRoutePlanningPeriod> findAll();
 
     SalesRoutePlanningPeriod findById(UUID salesRoutePlanningPeriodId);
+
+    SalesRoutePlanningPeriod getCurrentPlanPeriod(Date currentDate);
 }
