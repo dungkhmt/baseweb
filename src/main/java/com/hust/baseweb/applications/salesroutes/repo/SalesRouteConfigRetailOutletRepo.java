6254ac7671696add8253582410f72557a01bd431
AnhTuan-AiT <anhtuan0126104@gmail.com>
2020-06-10 14:33:36 +0700

    - modify: method getSalesRoutesConfigRetailOutlets

--- src/main/java/com/hust/baseweb/applications/salesroutes/repo/SalesRouteConfigRetailOutletRepo.java
+++ src/main/java/com/hust/baseweb/applications/salesroutes/repo/SalesRouteConfigRetailOutletRepo.java
@@ -2,6 +2,7 @@ package com.hust.baseweb.applications.salesroutes.repo;
 
 import com.hust.baseweb.applications.salesroutes.entity.SalesRouteConfigRetailOutlet;
 import com.hust.baseweb.applications.salesroutes.entity.SalesRoutePlanningPeriod;
+import com.hust.baseweb.applications.salesroutes.model.salesrouteconfigcustomer.GetSalesRouteConfigRetailOutletsOM;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,41 +13,24 @@ public interface SalesRouteConfigRetailOutletRepo extends JpaRepository<SalesRou
 
     List<SalesRouteConfigRetailOutlet> findBySalesRoutePlanningPeriod(SalesRoutePlanningPeriod salesRoutePlanningPeriod);
 
-    @Query( value = "select pro.retail_outlet_code retailOutletCode,\n" +
-                    "pro.retail_outlet_name retailOutletName,\n" +
-                    "ul.user_login_id salesmanName,\n" +
-                    "cast(ul.party_id as varchar) partySalesmanId,\n" +
-                    "pd.distributor_name distributorName,\n" +
-                    "srvf.description visitFrequency,\n" +
-                    "src.days visitConfig,\n" +
-                    "src.repeat_week repeatWeek\n" +
-                    "from sales_route_config_retail_outlet srcro\n" +
-                    "inner join sales_route_visit_frequency srvf on srcro.visit_frequency_id = srvf.visit_frequency_id\n" +
-                    "inner join sales_route_config  src on srcro.sales_route_config_id = src.sales_route_config_id \n" +
-                    "inner join retail_outlet_salesman_vendor rosv on srcro.retail_outlet_salesman_vendor_id = rosv.retail_outlet_salesman_vendor_id \n" +
-                    "inner join party_retail_outlet pro on rosv.party_retail_outlet_id = pro.party_id \n" +
-                    "inner join user_login ul on rosv.party_salesman_id = ul.party_id \n" +
-                    "inner join party_distributor pd on rosv.party_vendor_id = pd.party_id \n" +
+    @Query( value = "select cast(srcro.sales_route_config_retail_outlet_id as varchar) salesRouteConfigRetailOutletId, " +
+                    "pro.retail_outlet_code retailOutletCode, " +
+                    "pro.retail_outlet_name retailOutletName, " +
+                    "ul.user_login_id salesmanName, " +
+                    "cast(ul.party_id as varchar) partySalesmanId, " +
+                    "pd.distributor_name distributorName, " +
+                    "srvf.description visitFrequency, " +
+                    "src.days visitConfig, " +
+                    "src.repeat_week repeatWeek " +
+                    "from sales_route_config_retail_outlet srcro " +
+                    "inner join sales_route_visit_frequency srvf on srcro.visit_frequency_id = srvf.visit_frequency_id " +
+                    "inner join sales_route_config  src on srcro.sales_route_config_id = src.sales_route_config_id  " +
+                    "inner join retail_outlet_salesman_vendor rosv on srcro.retail_outlet_salesman_vendor_id = rosv.retail_outlet_salesman_vendor_id " +
+                    "inner join party_retail_outlet pro on rosv.party_retail_outlet_id = pro.party_id " +
+                    "inner join user_login ul on rosv.party_salesman_id = ul.party_id " +
+                    "inner join party_distributor pd on rosv.party_vendor_id = pd.party_id " +
                     "where srcro.sales_route_planning_period_id = ?1",
             nativeQuery = true)
-    List<GetSalesRouteConfigRetailOutletsOutputModel> getSalesroutesConfigRetailOutlets(UUID salesRoutePlanningPeriodId);
+    List<GetSalesRouteConfigRetailOutletsOM> getSalesRoutesConfigRetailOutlets(UUID salesRoutePlanningPeriodId);
 
-    interface GetSalesRouteConfigRetailOutletsOutputModel {
-
-        String getRetailOutletCode();
-
-        String getRetailOutletName();
-
-        String getSalesmanName();
-
-        String getPartySalesmanId();
-
-        String getDistributorName();
-
-        String getVisitFrequency();
-
-        String getVisitConfig();
-
-        int getRepeatWeek();
-    }
 }
