0b8699db90c89596b7a62c1a394ca709a853cf4b
AnhTuan-AiT <anhtuan0126104@gmail.com>
2020-06-10 14:32:45 +0700

    - add method: createSalesRouteConfig

--- src/main/java/com/hust/baseweb/applications/salesroutes/repo/SalesRouteConfigRepo.java
+++ src/main/java/com/hust/baseweb/applications/salesroutes/repo/SalesRouteConfigRepo.java
@@ -2,6 +2,9 @@ package com.hust.baseweb.applications.salesroutes.repo;
 
 import com.hust.baseweb.applications.salesroutes.entity.SalesRouteConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,4 +12,11 @@ import java.util.UUID;
 public interface SalesRouteConfigRepo extends JpaRepository<SalesRouteConfig, UUID> {
 
     List<SalesRouteConfig> findAll();
+
+    @Modifying
+    @Transactional
+    @Query(value = "insert into sales_route_config(visit_frequency_id , days , repeat_week) " +
+                   "values (?1, ?2, ?3)",
+           nativeQuery = true)
+    void createSalesRouteConfig(String visitFrequencyId, String days, int repeatWeek);
 }
