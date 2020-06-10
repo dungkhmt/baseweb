835aed0bb414e5948509a5845f2d9599e54e6dae
AnhTuan-AiT <anhtuan0126104@gmail.com>
2020-06-10 14:35:08 +0700

    - modify: method getSalesRoutesConfigRetailOutlets

--- src/main/java/com/hust/baseweb/applications/salesroutes/service/SalesRouteConfigRetailOutletService.java
+++ src/main/java/com/hust/baseweb/applications/salesroutes/service/SalesRouteConfigRetailOutletService.java
@@ -1,7 +1,7 @@
 package com.hust.baseweb.applications.salesroutes.service;
 
 import com.hust.baseweb.applications.salesroutes.entity.SalesRouteConfigRetailOutlet;
-import com.hust.baseweb.applications.salesroutes.repo.SalesRouteConfigRetailOutletRepo;
+import com.hust.baseweb.applications.salesroutes.model.salesrouteconfigcustomer.GetSalesRouteConfigRetailOutletsOM;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,7 +18,5 @@ public interface SalesRouteConfigRetailOutletService {
         String startExecuteDate
     );
 
-    List<SalesRouteConfigRetailOutletRepo.GetSalesRouteConfigRetailOutletsOutputModel> getSalesroutesConfigRetailOutlets(
-        UUID salesRoutePlanningPeriodId
-    );
+    List<GetSalesRouteConfigRetailOutletsOM> getSalesRoutesConfigRetailOutlets(UUID salesRoutePlanningPeriodId);
 }
