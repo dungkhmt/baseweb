aae6e727206b2a0cb3da20db2e81329254ac5944
AnhTuan-AiT <anhtuan0126104@gmail.com>
2020-06-26 16:28:19 +0700

    + add method: getVisitFrequencyId

--- src/main/java/com/hust/baseweb/applications/salesroutes/model/salesrouteconfigcustomer/GetSalesRouteConfigRetailOutletsOM.java
+++ src/main/java/com/hust/baseweb/applications/salesroutes/model/salesrouteconfigcustomer/GetSalesRouteConfigRetailOutletsOM.java
@@ -7,6 +7,7 @@ public interface GetSalesRouteConfigRetailOutletsOM {
     String getSalesmanName();
     String getPartySalesmanId();
     String getDistributorName();
+    String getVisitFrequencyId();
     String getVisitFrequency();
     String getVisitConfig();
     int getRepeatWeek();
