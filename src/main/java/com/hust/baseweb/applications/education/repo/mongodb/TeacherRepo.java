d8c08a0f33dd476aeeee2b07edf615246f0d5bc3
AnhTuan-AiT <anhtuan0126104@gmail.com>
2020-08-04 17:15:11 +0700

    create repo for interacting with collection "teacher"

---
+++ src/main/java/com/hust/baseweb/applications/education/repo/mongodb/TeacherRepo.java
@@ -0,0 +1,7 @@
+package com.hust.baseweb.applications.education.repo.mongodb;
+
+import com.hust.baseweb.applications.education.entity.mongodb.Teacher;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TeacherRepo extends MongoRepository<Teacher, String> {
+}
