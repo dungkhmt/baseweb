5c6af6e64276ccf7fc1c20e03e0c1a93c4d43a7d
tuandat95cbn <tuandat95cbn@gmail.com>
2020-08-18 00:23:40 +0700

    fix bug delete user still login

--- src/main/java/com/hust/baseweb/service/BaseWebUserDetailService.java
+++ src/main/java/com/hust/baseweb/service/BaseWebUserDetailService.java
@@ -3,7 +3,7 @@ package com.hust.baseweb.service;
 import com.hust.baseweb.entity.SecurityGroup;
 import com.hust.baseweb.entity.UserLogin;
 import com.hust.baseweb.repo.UserLoginRepo;
-import lombok.AllArgsConstructor;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
@@ -11,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -22,11 +24,10 @@ public class BaseWebUserDetailService implements UserDetailsService {
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
 
         UserLogin user = userLoginRepo.findByUserLoginId(s);
-        return new User(user.getUserLoginId(), user.getPassword(),
-                        AuthorityUtils.createAuthorityList(user
-                                                               .getRoles()
-                                                               .stream()
-                                                               .map(SecurityGroup::getGroupId)
-                                                               .toArray(String[]::new)));
+        if (user != null && user.isEnabled())
+            return new User(user.getUserLoginId(), user.getPassword(), AuthorityUtils.createAuthorityList(
+                    user.getRoles().stream().map(SecurityGroup::getGroupId).toArray(String[]::new)));
+        else
+            throw new UsernameNotFoundException("Username Not Found");
     }
 }
