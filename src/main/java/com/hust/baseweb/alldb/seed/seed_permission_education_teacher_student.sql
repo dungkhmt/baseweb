8142dab289350f259077d24fa854be710a455548
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2020-10-02 18:07:31 +0700

    set role for acc admin

--- src/main/java/com/hust/baseweb/alldb/seed/seed_permission_education_teacher_student.sql
+++ src/main/java/com/hust/baseweb/alldb/seed/seed_permission_education_teacher_student.sql
@@ -1,59 +1,67 @@
 -- group
-INSERT INTO public.security_group
+insert into public.security_group
     (group_id, description)
 VALUES ('ROLE_EDUCATION_TEACHING_MANAGEMENT_TEACHER', 'Group education: teacher');
 
-INSERT INTO public.security_group
+insert into public.security_group
     (group_id, description)
 VALUES ('ROLE_EDUCATION_LEARNING_MANAGEMENT_STUDENT', 'Group education: student');
 
 --permission
-INSERT INTO public.security_permission
+insert into public.security_permission
     (permission_id, description)
 VALUES ('EDUCATION_TEACHING_MANAGEMENT_TEACHER', 'Permission education teacher');
 
-INSERT INTO public.security_permission
+insert into public.security_permission
     (permission_id, description)
 VALUES ('EDUCATION_LEARNING_MANAGEMENT_STUDENT', 'Permission education student');
 
 --application
-INSERT INTO public.application
+insert into public.application
 (application_id, application_type_id, module_id, permission_id, description)
 VALUES ('MENU_EDUCATION_LEARNING_MANAGEMENT_STUDENT', 'MENU', NULL, 'EDUCATION_LEARNING_MANAGEMENT_STUDENT',
 'Menu learning management for students');
 
-INSERT INTO public.application
+insert into public.application
 (application_id, application_type_id, module_id, permission_id, description)
 VALUES ('MENU_EDUCATION_LEARNING_MANAGEMENT_STUDENT_REGISTER_CLASS', 'MENU', 'MENU_EDUCATION_LEARNING_MANAGEMENT_STUDENT', 'EDUCATION_LEARNING_MANAGEMENT_STUDENT',
 'Menu learning management for students: register to a class');
 
-INSERT INTO public.application
+insert into public.application
 (application_id, application_type_id, module_id, permission_id, description)
 VALUES ('MENU_EDUCATION_LEARNING_MANAGEMENT_STUDENT_VIEW_LIST_CLASS', 'MENU', 'MENU_EDUCATION_LEARNING_MANAGEMENT_STUDENT', 'EDUCATION_LEARNING_MANAGEMENT_STUDENT',
 'Menu learning management for students: view list of class');
 
 
-INSERT INTO public.application
+insert into public.application
 (application_id, application_type_id, module_id, permission_id, description)
 VALUES ('MENU_EDUCATION_TEACHING_MANAGEMENT_TEACHER', 'MENU', NULL, 'EDUCATION_TEACHING_MANAGEMENT_TEACHER',
 'Menu teaching management for teachers');
 
-INSERT INTO public.application
+insert into public.application
 (application_id, application_type_id, module_id, permission_id, description)
 VALUES ('MENU_EDUCATION_TEACHING_MANAGEMENT_TEACHER_VIEW_LIST_CLASS', 'MENU', 'MENU_EDUCATION_TEACHING_MANAGEMENT_TEACHER', 'EDUCATION_TEACHING_MANAGEMENT_TEACHER',
 'Menu teaching management for teachers: view list class');
 
-INSERT INTO public.application
+insert into public.application
 (application_id, application_type_id, module_id, permission_id, description)
 VALUES ('MENU_EDUCATION_TEACHING_MANAGEMENT_TEACHER_CREATE_CLASS', 'MENU', 'MENU_EDUCATION_TEACHING_MANAGEMENT_TEACHER', 'EDUCATION_TEACHING_MANAGEMENT_TEACHER',
 'Menu teaching management for teachers: create new class');
 
 -- group permission
-INSERT INTO public.security_group_permission
+insert into public.security_group_permission
     (group_id, permission_id)
 VALUES ('ROLE_EDUCATION_TEACHING_MANAGEMENT_TEACHER', 'EDUCATION_TEACHING_MANAGEMENT_TEACHER');
 
-INSERT INTO public.security_group_permission
+insert into public.security_group_permission
     (group_id, permission_id)
 VALUES ('ROLE_EDUCATION_LEARNING_MANAGEMENT_STUDENT', 'EDUCATION_LEARNING_MANAGEMENT_STUDENT');
+
+insert into user_login_security_group
+(user_login_id, group_id, last_updated_stamp, created_stamp)
+values('admin', 'ROLE_EDUCATION_TEACHING_MANAGEMENT_TEACHER', now(), now());
+
+insert into user_login_security_group
+(user_login_id, group_id, last_updated_stamp, created_stamp)
+values('admin', 'ROLE_EDUCATION_LEARNING_MANAGEMENT_STUDENT', now(), now());
 
