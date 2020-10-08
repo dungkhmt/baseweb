09e1cf9e3688e1e0d73a749b25394233c7664d87
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2020-10-08 15:49:50 +0700

    add extension 'unaccent'

--- src/main/java/com/hust/baseweb/alldb/extension.sql
+++ src/main/java/com/hust/baseweb/alldb/extension.sql
@@ -1,2 +1,12 @@
 --  uuid_generate_v1()
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
+
+-- Extension: unaccent
+
+-- DROP EXTENSION unaccent;
+
+CREATE EXTENSION unaccent
+	SCHEMA "public"
+	VERSION 1.1;
