ce8b2a5b7805a2f27686e1bb81aa89fa09491f0f
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2020-10-08 15:50:51 +0700

    add registered classes of student

--- src/main/java/com/hust/baseweb/applications/education/model/getclasslist/GetClassListOM.java
+++ src/main/java/com/hust/baseweb/applications/education/model/getclasslist/GetClassListOM.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.domain.Page;
 
+import java.util.Set;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -13,4 +15,6 @@ public class GetClassListOM {
     private short semesterId;
 
     private Page<ClassOM> page;
+
+    private Set<String> registeredClasses;
 }
