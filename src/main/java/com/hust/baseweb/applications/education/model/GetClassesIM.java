35044b2ce2306cd63671a24339829a8a5c05deca
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2020-10-14 19:54:14 +0700

    filter params for class registration

---
+++ src/main/java/com/hust/baseweb/applications/education/model/GetClassesIM.java
@@ -0,0 +1,19 @@
+package com.hust.baseweb.applications.education.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetClassesIM {
+
+    private Integer code;
+
+    private String courseId;
+
+    private String courseName;
+
+    private String classType;
+
+    private String departmentId;
+}
