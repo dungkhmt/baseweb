d287b7c940eed692e78796db352ee5451517468a
dungkhmt <dungkhmt@gmail.com>
2020-11-11 16:28:21 +0700

    update design teacher class assignment

--- src/main/java/com/hust/baseweb/applications/education/teacherclassassignment/model/AlgoInputClassModel.java
+++ src/main/java/com/hust/baseweb/applications/education/teacherclassassignment/model/AlgoInputClassModel.java
@@ -4,6 +4,7 @@ package com.hust.baseweb.applications.education.teacherclassassignment.model;
 public class AlgoInputClassModel {
     private String classId;
     private String className;
+    private String courseId;// ma mon hoc, vi du IT3011
     private String timetable; // example 1,41730,411145,7,8,9,11,12,13,B1-201;
 
     private double hourLoad; // gio quy doi cua lop (cd: 3 hours)
--- src/main/java/com/hust/baseweb/applications/education/teacherclassassignment/model/AlgoInputTeacherModel.java
+++ src/main/java/com/hust/baseweb/applications/education/teacherclassassignment/model/AlgoInputTeacherModel.java
@@ -3,5 +3,6 @@ package com.hust.baseweb.applications.education.teacherclassassignment.model;
 public class AlgoInputTeacherModel {
     private String teacherId;
     private String teacherName;
+    private String[] courseIds;// danh sach ma cac mon hoc ma giao vien co the day (file excel course4teacher)
     private double prespecifiedHourLoad; // so gio da duoc phan cong boi nhiem vu giang day khac
 }
