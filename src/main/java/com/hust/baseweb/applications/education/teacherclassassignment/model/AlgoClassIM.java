19b85d594208f9c030983b93d521e34c01490ed8
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2020-11-14 18:02:46 +0700

    modify model

--- src/main/java/com/hust/baseweb/applications/education/teacherclassassignment/model/AlgoClassIM.java
+++ src/main/java/com/hust/baseweb/applications/education/teacherclassassignment/model/AlgoClassIM.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Setter
 public class AlgoClassIM {
 
-    private String id;
+    private int id;
 
     private String classType;
 
--- src/main/java/com/hust/baseweb/applications/education/teacherclassassignment/model/Course4Teacher.java
+++ src/main/java/com/hust/baseweb/applications/education/teacherclassassignment/model/Course4Teacher.java
@@ -1,10 +1,12 @@
 package com.hust.baseweb.applications.education.teacherclassassignment.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Course4Teacher {
 
     private String courseId;
