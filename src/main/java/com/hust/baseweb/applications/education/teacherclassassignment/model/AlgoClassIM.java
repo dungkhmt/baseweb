085038fb6b1a5a2124b6c13e7aead367e372e7f8
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2020-11-15 19:37:18 +0700

    add attribute 'courseName'

--- src/main/java/com/hust/baseweb/applications/education/teacherclassassignment/model/AlgoClassIM.java
+++ src/main/java/com/hust/baseweb/applications/education/teacherclassassignment/model/AlgoClassIM.java
@@ -13,6 +13,8 @@ public class AlgoClassIM {
 
     private String courseId;// ma mon hoc, vi du IT3011
 
+    private String courseName;
+
     private String timetable; // example 1,41730,411145,7,8,9,11,12,13,B1-201;
 
     private double hourLoad; // gio quy doi cua lop (cd: 3 hours)
