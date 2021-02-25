bd9f2954d90393187fa8ee8031110b6b3d1a9ebc
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-02-25 21:55:18 +0700

    normalize APIs

--- src/main/java/com/hust/baseweb/applications/education/classmanagement/utils/ZipOutputStreamUtils.java
+++ src/main/java/com/hust/baseweb/applications/education/classmanagement/utils/ZipOutputStreamUtils.java
@@ -11,31 +11,47 @@ import java.util.List;
 
 public class ZipOutputStreamUtils {
 
-    public void zipOutputStream(
-        File outputZipFile, List<File> filesToAdd, char[] password,
-        CompressionMethod compressionMethod, boolean encrypt,
-        EncryptionMethod encryptionMethod, AesKeyStrength aesKeyStrength
-    )
-        throws IOException {
+    /**
+     * @param outputStream
+     * @param files             files to add
+     * @param compressionMethod
+     * @param password          to encrypt and decrypt the zip file. if the password is {@code null} or zero in length,
+     *                          the zip file will not be encrypted
+     * @param encryptionMethod
+     * @param aesKeyStrength
+     * @throws IOException
+     */
+    public static void zip(
+        OutputStream outputStream,
+        List<File> files,
+        CompressionMethod compressionMethod,
+        char[] password,
+        EncryptionMethod encryptionMethod,
+        AesKeyStrength aesKeyStrength
+    ) throws IOException {
+        boolean encrypt = true;
+
+        if (null == password || 0 == password.length) {
+            encrypt = false;
+        }
 
         ZipParameters zipParameters = buildZipParameters(compressionMethod, encrypt, encryptionMethod, aesKeyStrength);
         byte[] buff = new byte[4096];
         int readLen;
 
-        try (ZipOutputStream zos = initializeZipOutputStream(outputZipFile, encrypt, password)) {
-            for (File fileToAdd : filesToAdd) {
-
+        try (ZipOutputStream zos = initZipOutputStream(outputStream, encrypt, password)) {
+            for (File file : files) {
                 // Entry size has to be set if you want to add entries of STORE compression method (no compression)
                 // This is not required for deflate compression
                 if (zipParameters.getCompressionMethod() == CompressionMethod.STORE) {
-                    zipParameters.setEntrySize(fileToAdd.length());
+                    zipParameters.setEntrySize(file.length());
                 }
 
-                zipParameters.setFileNameInZip(fileToAdd.getName());
+                zipParameters.setFileNameInZip(file.getName());
                 zos.putNextEntry(zipParameters);
 
-                try (InputStream inputStream = new FileInputStream(fileToAdd)) {
-                    while ((readLen = inputStream.read(buff)) != -1) {
+                try (InputStream is = new FileInputStream(file)) {
+                    while ((readLen = is.read(buff)) != -1) {
                         zos.write(buff, 0, readLen);
                     }
                 }
@@ -45,28 +61,30 @@ public class ZipOutputStreamUtils {
         }
     }
 
-    private ZipOutputStream initializeZipOutputStream(File outputZipFile, boolean encrypt, char[] password)
-        throws IOException {
+    private static ZipOutputStream initZipOutputStream(
+        OutputStream os,
+        boolean encrypt,
+        char[] password
+    ) throws IOException {
+        return encrypt ? new ZipOutputStream(os, password) : new ZipOutputStream(os);
 
-        FileOutputStream fos = new FileOutputStream(outputZipFile);
-
-        if (encrypt) {
-            return new ZipOutputStream(fos, password);
-        }
-
-        return new ZipOutputStream(fos);
     }
 
-    private ZipParameters buildZipParameters(
-        CompressionMethod compressionMethod, boolean encrypt,
-        EncryptionMethod encryptionMethod, AesKeyStrength aesKeyStrength
+    private static ZipParameters buildZipParameters(
+        CompressionMethod compressionMethod,
+        boolean encrypt,
+        EncryptionMethod encryptionMethod,
+        AesKeyStrength aesKeyStrength
     ) {
         ZipParameters zipParameters = new ZipParameters();
 
         zipParameters.setCompressionMethod(compressionMethod);
-        zipParameters.setEncryptionMethod(encryptionMethod);
-        zipParameters.setAesKeyStrength(aesKeyStrength);
-        zipParameters.setEncryptFiles(encrypt);
+
+        if (encrypt) {
+            zipParameters.setEncryptFiles(encrypt);
+            zipParameters.setEncryptionMethod(encryptionMethod);
+            zipParameters.setAesKeyStrength(aesKeyStrength);
+        }
 
         return zipParameters;
     }
