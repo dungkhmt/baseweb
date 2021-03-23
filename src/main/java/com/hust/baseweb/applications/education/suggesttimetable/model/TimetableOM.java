6d7b91422e86366a19b790f374578f03a8528018
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-03-23 14:11:30 +0700

    add generated timetable output model

---
+++ src/main/java/com/hust/baseweb/applications/education/suggesttimetable/model/TimetableOM.java
@@ -0,0 +1,41 @@
+package com.hust.baseweb.applications.education.suggesttimetable.model;
+
+import com.hust.baseweb.applications.education.suggesttimetable.enums.Shift;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.DayOfWeek;
+
+/**
+ * @author Le Anh Tuan
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public class TimetableOM {
+
+    private final Integer classId;
+
+    private final Integer attachedClassId;
+
+    private final String courseId;
+
+    private final Integer credit;
+
+    private final DayOfWeek dayOfWeek;
+
+    private final Integer startTime;
+
+    private final Integer endTime;
+
+    private final Shift shift;
+
+    private final String weeks;
+
+    private final String room;
+
+    private final String classType;
+
+    private final String name;
+}
