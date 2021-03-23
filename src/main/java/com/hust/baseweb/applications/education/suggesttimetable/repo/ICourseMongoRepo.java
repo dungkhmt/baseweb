5c86ad07209290ca7cdf824a2092b890ac298458
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-03-23 18:29:47 +0700

    add method: findByIdIn()

--- src/main/java/com/hust/baseweb/applications/education/suggesttimetable/repo/ICourseMongoRepo.java
+++ src/main/java/com/hust/baseweb/applications/education/suggesttimetable/repo/ICourseMongoRepo.java
@@ -3,6 +3,10 @@ package com.hust.baseweb.applications.education.suggesttimetable.repo;
 import com.hust.baseweb.applications.education.suggesttimetable.entity.EduCourse;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+import java.util.Set;
+
 public interface ICourseMongoRepo extends MongoRepository<EduCourse, String> {
 
+    List<EduCourse> findByIdIn(Set<String> courseIds);
 }
--- src/main/java/com/hust/baseweb/applications/education/suggesttimetable/repo/ICourseRepo.java
+++ src/main/java/com/hust/baseweb/applications/education/suggesttimetable/repo/ICourseRepo.java
@@ -3,8 +3,11 @@ package com.hust.baseweb.applications.education.suggesttimetable.repo;
 import com.hust.baseweb.applications.education.suggesttimetable.entity.EduCourse;
 
 import java.util.List;
+import java.util.Set;
 
 public interface ICourseRepo {
+
     void saveAll(List<EduCourse> eduCourses);
 
+    List<EduCourse> findByIdIn(Set<String> courseIds);
 }
