15b909617a88702bbfe97197d94cf1d612a21abe
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-03-24 16:51:02 +0700

    change annotation to @RestController

--- src/main/java/com/hust/baseweb/applications/education/suggesttimetable/controller/SuggestTimetableController.java
+++ src/main/java/com/hust/baseweb/applications/education/suggesttimetable/controller/SuggestTimetableController.java
@@ -3,21 +3,16 @@ package com.hust.baseweb.applications.education.suggesttimetable.controller;
 import com.hust.baseweb.applications.education.exception.SimpleResponse;
 import com.hust.baseweb.applications.education.suggesttimetable.service.ISuggestTimeTableService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Set;
 
-@Controller
+@RestController
+@AllArgsConstructor
 @RequestMapping("/suggest-timetable")
-@AllArgsConstructor(onConstructor_ = @Autowired)
 public class SuggestTimetableController {
 
     private final ISuggestTimeTableService timeTableService;
