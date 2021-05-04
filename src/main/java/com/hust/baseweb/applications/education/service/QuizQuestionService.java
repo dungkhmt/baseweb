b2903969d3cfecfce3f0510683d79cadea0641ed
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-05-04 16:12:11 +0700

    refactor code

--- src/main/java/com/hust/baseweb/applications/education/service/QuizQuestionService.java
+++ src/main/java/com/hust/baseweb/applications/education/service/QuizQuestionService.java
@@ -5,20 +5,24 @@ import com.hust.baseweb.applications.education.model.quiz.QuizChooseAnswerInputM
 import com.hust.baseweb.applications.education.model.quiz.QuizQuestionCreateInputModel;
 import com.hust.baseweb.applications.education.model.quiz.QuizQuestionDetailModel;
 import com.hust.baseweb.entity.UserLogin;
-
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface QuizQuestionService {
-    public QuizQuestion save(QuizQuestionCreateInputModel input);
-    public QuizQuestion save(QuizQuestionCreateInputModel input, MultipartFile[] files);
 
-    public List<QuizQuestion> findAll();
-    public List<QuizQuestion> findQuizOfCourse(String courseId);
-    public QuizQuestionDetailModel findQuizDetail(UUID questionId);
-    public QuizQuestion changeOpenCloseStatus(UUID questionId);
+    QuizQuestion save(QuizQuestionCreateInputModel input);
 
-    public boolean checkAnswer(UserLogin userLogin, QuizChooseAnswerInputModel quizChooseAnswerInputModel);
+    QuizQuestion save(QuizQuestionCreateInputModel input, MultipartFile[] files);
+
+    List<QuizQuestion> findAll();
+
+    List<QuizQuestion> findQuizOfCourse(String courseId);
+
+    QuizQuestionDetailModel findQuizDetail(UUID questionId);
+
+    QuizQuestion changeOpenCloseStatus(UUID questionId);
+
+    boolean checkAnswer(UserLogin userLogin, QuizChooseAnswerInputModel quizChooseAnswerInputModel);
 }
