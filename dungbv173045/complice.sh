8520e3f4d225ed7b743cd9d4f292329593424a6c
dungbv99 <42971823+dungbv99@users.noreply.github.com>
2022-01-08 21:23:57 +0700

    Update complice.sh

--- dungbv173045/complice.sh
+++ dungbv173045/complice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mvn install:install-file -Dfile=or-tools_Ubuntu-20.04-64bit_v8.0.8283/ortools-java-8.0.8283.jar -DpomFile=or-tools_Ubuntu-20.04-64bit_v8.0.8283/pom-local.xml
-mvn install:install-file -Dfile=or-tools_Ubuntu-20.04-64bit_v8.0.8283/ortools-linux-x86-64-8.0.8283.jar -DpomFile=or-tools_Ubuntu-20.04-64bit_v8.0.8283/pom-runtime.xml
+mvn install:install-file -Dfile=or-tools_Ubuntu-20.04-64bit_v9.2.9972/ortools-java-9.2.9972.jar -DpomFile=or-tools_Ubuntu-20.04-64bit_v9.2.9972/pom-local.xml
+mvn install:install-file -Dfile=or-tools_Ubuntu-20.04-64bit_v9.2.9972/ortools-linux-x86-64-9.2.9972.jar -DpomFile=or-tools_Ubuntu-20.04-64bit_v9.2.9972/pom-runtime.xml
 mvn dependency:go-offline
 mvn install -DskipTests
