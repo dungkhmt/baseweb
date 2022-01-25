423b66e2f7320e1f627f42a5cfdf78bdb8754a43
Minh Hoàng <52208309+mHoang99@users.noreply.github.com>
2022-01-25 16:02:54 +0700

    Update 18-schema-chat.sql

--- src/main/java/com/hust/baseweb/alldb/schema/18-schema-chat.sql
+++ src/main/java/com/hust/baseweb/alldb/schema/18-schema-chat.sql
@@ -1,26 +1,34 @@
-create table char_group(
-    group_id uuid not null default uuid_generate_v1(),
-    group_name varchar(500)
-    created_by_user_login_id varchar(60),
-    last_updated_stamp         timestamp DEFAULT CURRENT_TIMESTAMP,
-    created_stamp              timestamp DEFAULT CURRENT_TIMESTAMP,
-
-    constraint pk_message_id primary key(msg_id)
-
+CREATE TABLE chat_group
+(
+    group_id uuid NOT NULL DEFAULT uuid_generate_v1(),
+    group_name character varying(500) COLLATE pg_catalog."default",
+    created_by_user_login_id character varying(60) COLLATE pg_catalog."default",
+    last_updated_stamp timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    created_stamp timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    last_message_time timestamp without time zone DEFAULT now(),
+    CONSTRAINT pk_group_id PRIMARY KEY (group_id)
 )
-create table user_login_chat_group(
-    user_login_id varchar(60),
+
+CREATE TABLE public.user_login_chat_group
+(
+    mapping_id uuid NOT NULL DEFAULT uuid_generate_v1(),
+    user_login_id character varying(60) COLLATE pg_catalog."default",
     group_id uuid,
+    CONSTRAINT pk_mapping_id PRIMARY KEY (mapping_id)
 )
-create table message(
-    msg_id uuid not null default uuid_generate_v1(),
-    message text,
-    created_by_user_login_id varchar(60),
-    to_user_login_id varchar(60),
+
+
+CREATE TABLE message
+(
+    msg_id uuid NOT NULL DEFAULT uuid_generate_v1(),
+    message text COLLATE pg_catalog."default",
+    created_by_user_login_id character varying(60) COLLATE pg_catalog."default",
+    to_user_login_id character varying(60) COLLATE pg_catalog."default",
     to_group_id uuid,
     reply_to_message_id uuid,
-    status_id varchar(200),
-    last_updated_stamp         timestamp DEFAULT CURRENT_TIMESTAMP,
-    created_stamp              timestamp DEFAULT CURRENT_TIMESTAMP,
-    constraint pk_message_id primary key(msg_id)
+    status_id character varying(200) COLLATE pg_catalog."default",
+    last_updated_stamp timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    created_stamp timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_message_id PRIMARY KEY (msg_id)
 )
+
